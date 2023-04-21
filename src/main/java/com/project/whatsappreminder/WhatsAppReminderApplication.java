87f6e7538af5f3a5f215cc6081c0d4e78a531bf3
fynnbrock <fynn.brock@students.ffhs.ch>
2023-04-21 16:58:25 +0200

    init

---
+++ src/main/java/com/project/whatsappreminder/WhatsAppReminderApplication.java
@@ -0,0 +1,13 @@
+package com.project.whatsappreminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsAppReminderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WhatsAppReminderApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/project/whatsappreminder/WhatsAppReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.whatsappreminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppReminderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
